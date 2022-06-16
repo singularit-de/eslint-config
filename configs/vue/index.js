ed3d3ba93d3860e96852df9a552527566cc1459b
Robin <robin.kehl@singular-it.de>
2022-06-16 09:41:07 +0200

    chore: add release command

--- configs/vue/index.js
+++ configs/vue/index.js
@@ -1,5 +1,21 @@
 module.exports = {
-    extends: [
-        // extend other configs
+    overrides: [
+        {
+            files: ['*.vue'],
+            parser: 'vue-eslint-parser',
+            parserOptions: {
+                parser: '@typescript-eslint/parser',
+            },
+            rules: {
+                'no-unused-vars': 'off',
+                'no-undef': 'off',
+                '@typescript-eslint/no-unused-vars': 'off',
+            },
+        },
     ],
+    extends: [
+        'plugin:vue/vue3-recommended',
+        // TODO: @singularit/eslint-plugin-ts
+    ],
+    rules: {}
 }
