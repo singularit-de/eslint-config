32ec4e6d82ba917a59bef4eeb38796d1b5fd5a39
Tim Streicher <tim.streicher@singular-it.de>
2022-07-05 13:24:39 +0200

    feat: add rules and overrides

--- configs/vue/index.js
+++ configs/vue/index.js
@@ -1,20 +1,21 @@
 module.exports = {
-    overrides: [
-        {
-            files: ['*.vue'],
-            parser: 'vue-eslint-parser',
-            parserOptions: {
-                parser: '@typescript-eslint/parser',
-            },
-            rules: {
-                'no-unused-vars': 'off',
-                'no-undef': 'off',
-                '@typescript-eslint/no-unused-vars': 'off',
-            },
-        },
-    ],
-    extends: [
-        'plugin:vue/vue3-recommended',
-    ],
-    rules: {}
+  overrides: [
+    {
+      files: ['*.vue'],
+      parser: 'vue-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+      },
+      rules: {
+        'no-unused-vars': 'off',
+        'no-undef': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+      },
+    },
+  ],
+  extends: [
+    'plugin:vue/vue3-recommended',
+    '@singularit/eslint-config-typescript',
+  ],
+  rules: {},
 }
